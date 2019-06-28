a1ebebe193352923035c81c4f46aedd0fae6e95d
FernonE <fernon94@hotmail.com>
2019-06-28 17:14:36 +0200

    Added all leds

--- src/main/java/com/led/BlinkingLed/BlinkingLedApplication.java
+++ src/main/java/com/led/BlinkingLed/BlinkingLedApplication.java
@@ -1,13 +1,68 @@
 package com.led.BlinkingLed;
 
+import com.pi4j.io.gpio.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlinkingLedApplication {
 
+	private static GpioPinDigitalOutput greenLed0;
+	private static GpioPinDigitalOutput yellowLed0;
+	private static GpioPinDigitalOutput yellowLed1;
+	private static GpioPinDigitalOutput redLed0;
+	private static GpioPinDigitalOutput redLed1;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BlinkingLedApplication.class, args);
+
+		GpioController gpioController = GpioFactory.getInstance();
+		greenLed0 = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_01, "greenLed0", PinState.LOW);
+		yellowLed0 = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_04, "yellowLed0", PinState.LOW);
+		yellowLed0 = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_05, "yellowLed1", PinState.LOW);
+		redLed0 = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_06, "redLed0", PinState.LOW);
+		redLed1 = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_10, "redLed1", PinState.LOW);
 	}
 
+
+	/* ---------- Getters and Setters ---------- */
+	public static GpioPinDigitalOutput getGreenLed0() {
+		return greenLed0;
+	}
+
+	public static void setGreenLed0(GpioPinDigitalOutput greenLed0) {
+		BlinkingLedApplication.greenLed0 = greenLed0;
+	}
+
+	public static GpioPinDigitalOutput getYellowLed0() {
+		return yellowLed0;
+	}
+
+	public static void setYellowLed0(GpioPinDigitalOutput yellowLed0) {
+		BlinkingLedApplication.yellowLed0 = yellowLed0;
+	}
+
+	public static GpioPinDigitalOutput getYellowLed1() {
+		return yellowLed1;
+	}
+
+	public static void setYellowLed1(GpioPinDigitalOutput yellowLed1) {
+		BlinkingLedApplication.yellowLed1 = yellowLed1;
+	}
+
+	public static GpioPinDigitalOutput getRedLed0() {
+		return redLed0;
+	}
+
+	public static void setRedLed0(GpioPinDigitalOutput redLed0) {
+		BlinkingLedApplication.redLed0 = redLed0;
+	}
+
+	public static GpioPinDigitalOutput getRedLed1() {
+		return redLed1;
+	}
+
+	public static void setRedLed1(GpioPinDigitalOutput redLed1) {
+		BlinkingLedApplication.redLed1 = redLed1;
+	}
 }
--- src/main/java/com/led/BlinkingLed/api/LedController.java
+++ src/main/java/com/led/BlinkingLed/api/LedController.java
@@ -1,44 +1,80 @@
 package com.led.BlinkingLed.api;
 
-import com.pi4j.io.gpio.*;
-import com.mgnt.utils.TimeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.TimeUnit;
+import static com.led.BlinkingLed.BlinkingLedApplication.*;
 
 @RestController
 @RequestMapping("/led")
 public class LedController {
 
-    private static GpioPinDigitalOutput pin;
 
-
-    @GetMapping("/on")
-    public String ledOn (){
-        System.out.println("Turning led on");
-        if (pin == null){
-            GpioController gpioController = GpioFactory.getInstance();
-            pin = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_01, "greenLed", PinState.LOW);
-        }
-        pin.high();
-
-        System.out.println("Done");
-        return "Turned led on";
+    @GetMapping("/greenLed0/On")
+    public void greenLed0On (){
+        getGreenLed0().high();
     }
 
-    @GetMapping("/off")
-    public String ledoff (){
-        System.out.println("Turning led off");
-        if (pin == null){
-            GpioController gpioController = GpioFactory.getInstance();
-            pin = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_01, "greenLed", PinState.LOW);
-        }
+    @GetMapping("/greenLed0/Off")
+    public void greenLed0Off () {
+        getGreenLed0().low();
+    }
 
-        pin.low();
+    @GetMapping("/yellowLed0/On")
+    public void yellowLed0On (){
+        getYellowLed0().high();
+    }
 
-        System.out.println("Done");
-        return "Turned led off";
+    @GetMapping("/yellowLed0/Off")
+    public void yellowLed0Off () {
+        getYellowLed0().low();
+    }
+
+    @GetMapping("/yellowLed1/On")
+    public void yellowLed1On (){
+        getYellowLed1().high();
+    }
+
+    @GetMapping("/yellowLed1/Off")
+    public void yellowLed1Off () {
+        getYellowLed1().low();
+    }
+
+    @GetMapping("/redLed0/On")
+    public void redLed0On (){
+        getRedLed0().high();
+    }
+
+    @GetMapping("/redLed0/Off")
+    public void redLed0Off () {
+        getRedLed0().low();
+    }
+
+    @GetMapping("/redLed1/On")
+    public void redLed1On (){
+        getRedLed1().high();
+    }
+
+    @GetMapping("/redLed1/Off")
+    public void redLed1Off () {
+        getRedLed1().low();
+    }
+
+    @GetMapping("/AllOn")
+    public void allOn (){
+        greenLed0On();
+        yellowLed0On();
+        yellowLed1On();
+        redLed0On();
+        redLed1On();
+    }
+
+    @GetMapping("/AllOff")
+    public void allOff () {
+        greenLed0Off();
+        yellowLed0Off();
+        yellowLed1Off();
+        redLed0Off();
+        redLed1Off();
     }
 }
